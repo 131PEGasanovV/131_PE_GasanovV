61268bbb873abcf35508636c4e376598526122d8
131PEGasanovV <ghost777796@mail.ru>
2016-02-08 12:31:31 +0300

    Лабораторная 1, задание 1, расчет дневной оплаты

---
+++ 131_PE_GasanovV/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _131_PE_GasanovV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("Введите дневную выручку продовца");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            if (x <= 10000)
+                Console.WriteLine("Выручка =", 0.05 * x);
+            else
+                Console.WriteLine("Выручка = {0} ", (10000 * 0.05) + ((x - 10000) * 0.1));
+            Console.ReadKey();
+        }
+    }
+}
